96c1d7981390f9e25fea89281c7a3181b1b6647c
adindacoding <adindabeer@gmail.com>
2024-01-17 14:50:49 +0100

    Initial commit

---
+++ src/main/java/adinda/stroopwafel/Stroopwafel.java
@@ -0,0 +1,16 @@
+package adinda.stroopwafel;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Stroopwafel implements ModInitializer {
+	public static final String MOD_ID = "stroopwafel";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/adinda/stroopwafel/StroopwafelDataGenerator.java
@@ -0,0 +1,11 @@
+package adinda.stroopwafel;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class StroopwafelDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
